80d61417738cf3ccfadf834270bc2eefd5644beb
cdc-ap66 <ap66@cdc.gov>
2025-10-06 19:01:32 +0000

    pep8

--- cfa/catalog/public/__init__.py
+++ cfa/catalog/public/__init__.py
@@ -2,18 +2,27 @@ from os import path
 
 _here = path.abspath(path.dirname(__file__))
 _parent = path.dirname(_here)
-_dataops_creation_version = '2025.10.3.0a0'
-_catalog_ns = 'public'
+_dataops_creation_version = "2025.10.3.0a0"
+_catalog_ns = "public"
+
 
 def get_version_separation():
     try:
         from cfa.dataops import __version__ as _installed_dataops_version
+
         print(
             f"dataops version at catalog creation: {_dataops_creation_version}",
             f"installed dataops version: {_installed_dataops_version}",
-            sep="\n"
+            sep="\n",
         )
     except ImportError:
         pass
 
-__all__ = [_here, _parent, _dataops_creation_version, _catalog_ns, get_version_separation]
+
+__all__ = [
+    _here,
+    _parent,
+    _dataops_creation_version,
+    _catalog_ns,
+    get_version_separation,
+]
