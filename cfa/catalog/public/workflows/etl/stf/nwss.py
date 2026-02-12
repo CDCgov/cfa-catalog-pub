9374031b5cfbcbc67482e91c411f34c0da3ebfa3
Ryan Raasch <xng3@cdc.gov>
2026-02-12 21:58:04 +0000

    updated workflow

--- cfa/catalog/public/workflows/etl/stf/nwss.py
+++ cfa/catalog/public/workflows/etl/stf/nwss.py
@@ -1,3 +1,4 @@
+import re
 from types import SimpleNamespace
 
 import polars as pl
@@ -11,23 +12,25 @@ source_blob = SimpleNamespace(**dataset.config["source"]["storage_location"])
 
 
 def copy_missing_files() -> None:
-    nssp_files = sorted(
+    nwss_files = sorted(
         [
-            i["name"].removeprefix(source_blob.prefix).removesuffix("/")
+            i["name"].removesuffix("/")
             for i in walk_blobs_in_container(
                 account_name=source_blob.account,
                 container_name=source_blob.container,
-                name_starts_with=source_blob.prefix,
             )
         ],
         reverse=True,
     )
     cached_versions = dataset.load.get_versions()
 
+    date_pattern = re.compile(r"(\d{4}-\d{2}-\d{2})")
+
     files_to_copy = [
         i
-        for i in nssp_files
-        if i.removesuffix(".parquet") not in cached_versions
+        for i in nwss_files
+        if i.startswith("NWSS-ETL-covid")
+        and re.search(date_pattern, i).group(1) not in cached_versions
     ]
 
     if not files_to_copy:
@@ -41,14 +44,15 @@ def copy_missing_files() -> None:
             read_blob_stream(
                 account_name=source_blob.account,
                 container_name=source_blob.container,
-                blob_url=f'{source_blob.prefix.removesuffix("/")}/{file}',
+                blob_url=f"{file}/bronze.parquet",
             ).content_as_bytes()
         )
 
         # if there is some transformation to the original data that should be performed before saving
         # make those transformations to the dataframe here
+        date = re.search(date_pattern, file).group(1)
 
         dataset.load.save_dataframe(
             df,
-            path_after_prefix=f"{file.removesuffix('.parquet')}/data.parquet",
+            path_after_prefix=f"{date}/data.parquet",
         )
