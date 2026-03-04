1308f10d5afa6fdb0c95afdcdbcc3a7614b49e6d
Ryan Raasch <xng3@cdc.gov>
2026-03-04 21:09:16 +0000

    fix name

--- cfa/catalog/public/workflows/etl/stf/nssp_latest_comprehensive.py
+++ cfa/catalog/public/workflows/etl/stf/nssp_latest_comprehensive.py
@@ -1,25 +1,25 @@
-from types import SimpleNamespace
 from datetime import datetime
+from types import SimpleNamespace
+
 import polars as pl
 
-
-from cfa.cloudops.blob_helpers import read_blob_stream, walk_blobs_in_container
+from cfa.cloudops.blob_helpers import read_blob_stream
 from cfa.dataops import datacat
-
 
 dataset = datacat.public.stf.nssp_latest_comprehensive
 source_blob = SimpleNamespace(**dataset.config["source"]["storage_location"])
 
 file_to_copy = "latest_comprehensive.parquet"
 
+
 def copy_file() -> None:
     df = pl.read_parquet(
-            read_blob_stream(
-                account_name=source_blob.account_name,
-                container_name=source_blob.container_name,
-                blob_url=file_to_copy,
-            ).content_as_bytes()
-        )
+        read_blob_stream(
+            account_name=source_blob.account,
+            container_name=source_blob.container,
+            blob_url=file_to_copy,
+        ).content_as_bytes()
+    )
 
     date = datetime.now().isoformat().split("T")[0]
     dataset.load.save_dataframe(
